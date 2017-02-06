ae9bf608e6c6d989b3f8701383e29dc46c1786d0
千里草言寺心情 <>
2017-02-06 15:38:52 +0800

    20170206_02

--- code/008-练习-购物车/app/index.js
+++ code/008-练习-购物车/app/index.js
@@ -5,26 +5,95 @@ cartApp.controller('cartCtrl', function($scope){
         {
             id: 1000,
             name: 'iphone5s',
-            quantity: '3',
+            quantity: 3,
             price: 4300
         },
         {
             id: 33000,
             name: 'iphone6s',
-            quantity: '20',
+            quantity: 20,
             price: 6300
         },
         {
             id: 232,
             name: 'iphone7',
-            quantity: '23',
+            quantity: 23,
             price: 6500
         },
         {
             id: 5444,
             name: 'iphone7s',
-            quantity: '10',
+            quantity: 10,
             price: 6900
         }
     ];
+    /**
+    * 计算购物总价
+    * */
+    $scope.totalPrice = function(){
+        var total = 0;
+        angular.forEach($scope.cart, function(item){
+            total += item.quantity * item.price;
+        });
+        return total;
+    }
+    /**
+     *计算总购买数
+     */
+    $scope.totalQuantity = function(){
+        var total = 0;
+        angular.forEach($scope.cart, function(item){
+            total += parseInt(item.quantity);
+        });
+        return total;
+    }
+    /**
+     * 找一个元素的索引
+     */
+    var findIndex = function (id) {
+        var index = -1;
+        angular.forEach($scope.cart, function (item, key) {
+            if(id == item.id){
+                index = key;
+                return;
+            }
+        });
+        return index;
+    }
+    /**
+     * 移除一项
+     */
+    $scope.remove = function(id){
+
+        var index = findIndex(id);
+        if (index != -1){
+            $scope.cart.splice(index, 1);
+        }
+    }
+    /**
+     * 为某个产品添加数量
+     */
+    $scope.add = function (id) {
+        var index = findIndex(id);
+        if (index != -1){
+            ++$scope.cart[index].quantity;
+        }
+    }
+    /**
+     * 为某个产品减少数量
+     */
+    $scope.reduce = function (id) {
+        var index = findIndex(id);
+        if (index != -1){
+            var item = $scope.cart[index];
+            if (item.quantity > 1){
+                --item.quantity;
+            }else{
+                var returnKey = confirm("从购物车内删除该产品！");
+                if (returnKey){
+                    $scope.remove(id);
+                }
+            }
+        }
+    }
 });
