bdb8220d3b76e67531885070ce26dce7bec7a405
千里草言寺心情 <>
2017-02-07 09:11:14 +0800

    20160207_01

--- code/014-$provide里factory、service方法/app/index.js
+++ code/014-$provide里factory、service方法/app/index.js
@@ -18,7 +18,7 @@ var app = angular.module('myApp', [], function ($provide) {
         return [1,2,3,4,5,6,7,8,9];
     });
 
-    //自定义服务
+    //自定义服务,和factory类似，但是它的返回类型必须是object，不能是基本类型，返回字符串的话会报错
     $provide.service('CustomService2', function () {
         return ['上海', '北京', '大连'];
     });
---
+++ code/016-过滤器/app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dongs on 1/25/2017.
+ */
+
+var app = angular.module('myApp', []);
+
+app.controller('firstCtrl', function ($scope) {
+    $scope.numberTest = 123456789;
+});
